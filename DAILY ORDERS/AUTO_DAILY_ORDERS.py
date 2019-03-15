b02a399265e8f884f9b5b3cf8323364c8ed26257
Jan Zalobinski <39808183+fragrarach@users.noreply.github.com>
2019-03-15 10:05:31 -0400

    Removed old DEV files, refactored to use functions, added comments, added TODOs

--- DAILY ORDERS/AUTO_DAILY_ORDERS.py
+++ DAILY ORDERS/AUTO_DAILY_ORDERS.py
@@ -1,6 +1,5 @@
 import smtplib
 import psycopg2.extensions
-import re
 import datetime
 import os
 import shutil
@@ -10,7 +9,23 @@ from email.mime.multipart import MIMEMultipart
 from email.mime.text import MIMEText
 from email.mime.application import MIMEApplication
 
-conn_sigm = psycopg2.connect("host='192.168.0.250' dbname='QuatroAir' user='SIGM' port='5493'")
+
+# Check whether app should reference dev or prod server/db
+def dev_check():
+    raw_filename = os.path.basename(__file__)
+    removed_extension = raw_filename.split('.')[0]
+    last_word = removed_extension.split('_')[-1]
+    if last_word == 'DEV':
+        return True
+    else:
+        return False
+
+
+# PostgreSQL DB connection configs
+if dev_check():
+    conn_sigm = psycopg2.connect("host='192.168.0.57' dbname='DEV' user='SIGM' port='5493'")
+else:
+    conn_sigm = psycopg2.connect("host='192.168.0.250' dbname='QuatroAir' user='SIGM' port='5493'")
 conn_sigm.set_client_encoding("latin1")
 conn_sigm.set_isolation_level(psycopg2.extensions.ISOLATION_LEVEL_AUTOCOMMIT)
 
@@ -24,10 +39,12 @@ log_query = conn_log.cursor()
 
 parent_dir = dirname(abspath(__file__))
 
+# HTML to PDF convert location
 path_wkthmltopdf = r'C:\Program Files\wkhtmltopdf\bin\wkhtmltopdf.exe'
 config = pdfkit.configuration(wkhtmltopdf=path_wkthmltopdf)
 
 
+# Change alignment in HTML file, expandable
 def format_html(html_path):
     with open(html_path, "r") as file:
         file_data = file.read()
@@ -37,6 +54,8 @@ def format_html(html_path):
         file.write(file_data)
 
 
+# TODO : Convert attachments to list or dict to allow for multiple attachments
+# Send formatted email body to defined recipients, include attachment if exists
 def send_email(email_body, salesman, attachment=None, attachment_name=None):
     from_str = 'noreply@quatroair.com'
     to_list = [salesman]
@@ -67,12 +86,14 @@ def send_email(email_body, salesman, attachment=None, attachment_name=None):
     s.quit()
 
 
+# Run VBS script which runs Excel (VBA) file, which generates HTML files
 def html_generator():
     vbs_file = r'\VBA\DAILY ORDERS.vbs'
     vbs_path = f'{parent_dir}{vbs_file}'
     os.system(f'"{vbs_path}"')
 
 
+# Insert HTML files into email body, clean HTML folders, generate PDFs, send emails
 def email_handler():
     salesmen_list = ['MARK STACHOWSKI', 'GREG PHILLIPS']
     grouping_list = ['NEW', 'QUOTES', 'PENDING', 'UPDATED', 'UPDATED QUOTES']
@@ -80,6 +101,8 @@ def email_handler():
     for salesman in salesmen_list:
         email_body = ''
         for grouping in grouping_list:
+
+            # TODO : Convert this block to an email body generator function
             header_file = f'{parent_dir}\\HTML\\{grouping}.html'
             html_file = f'\\VBA\\{salesman} {grouping}.html'
             html_folder = f'\\VBA\\{salesman} {grouping}_files\\'
@@ -96,6 +119,7 @@ def email_handler():
                 shutil.rmtree(html_folder_path)
 
         if email_body != '':
+            # TODO : Convert this block into a PDF generator function
             time_stamp = datetime.datetime.now().strftime('%H00-%d-%m-%Y')
             pdf_name = f'Daily Orders ({time_stamp}).pdf'
             pdf_file = f'{parent_dir}\\PDF\\{pdf_name}'
@@ -116,6 +140,8 @@ def email_handler():
                     send_email(email_body, 'greg.p@quatroair.com')
 
 
+# TODO : Add a 'sent' boolean column to daily_orders log table instead of deleting sent orders
+# Clear log of orders that are already sent, add orders to log that have just been sent
 def exclusion_log():
     sql_exp = f'DELETE FROM daily_orders'
     log_query.execute(sql_exp)
@@ -134,12 +160,19 @@ def exclusion_log():
                 log_query.execute(sql_exp)
 
 
+# TODO : Add a 'sent' boolean column to daily_orders_updated log table instead of deleting sent updated orders
+# Clear log of updated orders
 def clear_updated():
     sql_exp = f'DELETE FROM daily_orders_updated'
     log_query.execute(sql_exp)
 
 
-html_generator()
-email_handler()
-exclusion_log()
-clear_updated()
+def main():
+    html_generator()
+    email_handler()
+    if not dev_check():
+        exclusion_log()
+        clear_updated()
+
+
+main()
--- DAILY ORDERS/LISTEN_DAILY_ORDERS.py
+++ DAILY ORDERS/LISTEN_DAILY_ORDERS.py
@@ -1,18 +1,26 @@
 import os
-import select
-import datetime
-import sys
-import re
-import ast
-import psycopg2
-import psycopg2.extras
 import psycopg2.extensions
-from os.path import dirname, abspath
 
+
+# Check whether app should reference dev or prod server/db
+def dev_check():
+    raw_filename = os.path.basename(__file__)
+    removed_extension = raw_filename.split('.')[0]
+    last_word = removed_extension.split('_')[-1]
+    if last_word == 'DEV':
+        return True
+    else:
+        return False
+
+
+# PostgreSQL DB connection configs
 psycopg2.extensions.register_type(psycopg2.extensions.UNICODE)
 psycopg2.extensions.register_type(psycopg2.extensions.UNICODEARRAY)
 
-conn_sigm = psycopg2.connect("host='192.168.0.250' dbname='QuatroAir' user='SIGM' port='5493'")
+if dev_check():
+    conn_sigm = psycopg2.connect("host='192.168.0.57' dbname='DEV' user='SIGM' port='5493'")
+else:
+    conn_sigm = psycopg2.connect("host='192.168.0.250' dbname='QuatroAir' user='SIGM' port='5493'")
 conn_sigm.set_client_encoding("latin1")
 conn_sigm.set_isolation_level(psycopg2.extensions.ISOLATION_LEVEL_AUTOCOMMIT)
 
@@ -26,45 +34,98 @@ conn_log.set_isolation_level(psycopg2.extensions.ISOLATION_LEVEL_AUTOCOMMIT)
 
 log_query = conn_log.cursor()
 
-while 1:
-    conn_sigm.poll()
-    conn_sigm.commit()
-    while conn_sigm.notifies:
-        notify = conn_sigm.notifies.pop()
-        raw_payload = notify.payload
 
-        change_type = raw_payload.split(', ')[0]
-        ord_no = raw_payload.split(', ')[1]
+# Split base variables from payload string, return named variables
+def base_payload_handler(payload):
+    change_type = payload.split(', ')[0]
+    ord_no = payload.split(', ')[1]
+    return change_type, ord_no
 
-        if change_type == 'CONVERTED QUOTE':
-            sql_exp = f'INSERT INTO daily_orders_updated ' \
-                      f'(change_type, ord_no) ' \
-                      f'VALUES (\'{change_type}\', {ord_no})'
-            print(sql_exp)
-            log_query.execute(sql_exp)
-        elif change_type == 'ADDED PART':
-            orl_id = raw_payload.split(', ')[2]
-            sql_exp = f'INSERT INTO daily_orders_updated ' \
-                      f'(change_type, ord_no, orl_id) ' \
-                      f'VALUES (\'{change_type}\', {ord_no}, {orl_id})'
-            print(sql_exp)
-            log_query.execute(sql_exp)
-        elif change_type == 'PRICE CHANGED':
-            orl_id = raw_payload.split(', ')[2]
-            orl_price = raw_payload.split(', ')[3]
-            sql_exp = f'INSERT INTO daily_orders_updated ' \
-                      f'(change_type, ord_no, orl_id, orl_price) ' \
-                      f'VALUES (\'{change_type}\', {ord_no}, {orl_id}, {orl_price})'
-            print(sql_exp)
-            log_query.execute(sql_exp)
-        elif change_type == 'REMOVED PART':
-            orl_id = raw_payload.split(', ')[2]
-            orl_price = raw_payload.split(', ')[3]
-            prt_no = raw_payload.split(', ')[4]
-            orl_quantity = raw_payload.split(', ')[5]
-            prt_dscnt = raw_payload.split(', ')[6]
-            sql_exp = f'INSERT INTO daily_orders_updated ' \
-                      f'(change_type, ord_no, orl_id, orl_price, prt_no, orl_quantity, prt_dscnt) ' \
-                      f'VALUES (\'{change_type}\', {ord_no}, {orl_id}, {orl_price}, \'{prt_no}\', {orl_quantity}, {prt_dscnt})'
-            print(sql_exp)
-            log_query.execute(sql_exp)
+
+# Split added part variables from payload string, return named variables
+def added_part_payload_handler(payload):
+    orl_id = payload.split(', ')[2]
+    return orl_id
+
+
+# Split price changed variables from payload string, return named variables
+def price_changed_payload_handler(payload):
+    orl_id = payload.split(', ')[2]
+    orl_price = payload.split(', ')[3]
+    return orl_id, orl_price
+
+
+# Split removed part variables from payload string, return named variables
+def removed_part_payload_handler(payload):
+    orl_id = payload.split(', ')[2]
+    orl_price = payload.split(', ')[3]
+    prt_no = payload.split(', ')[4]
+    orl_quantity = payload.split(', ')[5]
+    prt_dscnt = payload.split(', ')[6]
+    return orl_id, orl_price, prt_no, orl_quantity, prt_dscnt
+
+
+# Insert converted quote data into 'daily_orders_updated' table
+def converted_quote(change_type, ord_no):
+    sql_exp = f'INSERT INTO daily_orders_updated ' \
+              f'(change_type, ord_no) ' \
+              f'VALUES (\'{change_type}\', {ord_no})'
+    print(sql_exp)
+    log_query.execute(sql_exp)
+
+
+# Insert added part data into 'daily_orders_updated' table
+def added_part(change_type, ord_no, orl_id):
+    sql_exp = f'INSERT INTO daily_orders_updated ' \
+              f'(change_type, ord_no, orl_id) ' \
+              f'VALUES (\'{change_type}\', {ord_no}, {orl_id})'
+    print(sql_exp)
+    log_query.execute(sql_exp)
+
+
+# Insert price changed data into 'daily_orders_updated' table
+def price_changed(change_type, ord_no, orl_id, orl_price):
+    sql_exp = f'INSERT INTO daily_orders_updated ' \
+              f'(change_type, ord_no, orl_id, orl_price) ' \
+              f'VALUES (\'{change_type}\', {ord_no}, {orl_id}, {orl_price})'
+    print(sql_exp)
+    log_query.execute(sql_exp)
+
+
+# Insert removed part data into 'daily_orders_updated' table
+def removed_part(change_type, ord_no, orl_id, orl_price, prt_no, orl_quantity, prt_dscnt):
+    sql_exp = f'INSERT INTO daily_orders_updated ' \
+              f'(change_type, ord_no, orl_id, orl_price, prt_no, orl_quantity, prt_dscnt) ' \
+              f'VALUES (\'{change_type}\', {ord_no}, {orl_id}, {orl_price}, \'{prt_no}\', {orl_quantity}, {prt_dscnt})'
+    print(sql_exp)
+    log_query.execute(sql_exp)
+
+
+# TODO : Catch connection error during DB service downtime
+def main():
+    while 1:
+        conn_sigm.poll()
+        conn_sigm.commit()
+        while conn_sigm.notifies:
+            notify = conn_sigm.notifies.pop()
+            raw_payload = notify.payload
+
+            change_type, ord_no = base_payload_handler(raw_payload)
+
+            if change_type == 'CONVERTED QUOTE':
+                converted_quote(change_type, ord_no)
+
+            elif change_type == 'ADDED PART':
+                orl_id = added_part_payload_handler(raw_payload)
+                added_part(change_type, ord_no, orl_id)
+
+            elif change_type == 'PRICE CHANGED':
+                orl_id, orl_price = price_changed_payload_handler(raw_payload)
+                price_changed(change_type, ord_no, orl_id, orl_price)
+
+            elif change_type == 'REMOVED PART':
+                orl_id, orl_price, prt_no, orl_quantity, prt_dscnt = removed_part_payload_handler(raw_payload)
+                removed_part(change_type, ord_no, orl_id, orl_price, prt_no, orl_quantity, prt_dscnt)
+
+
+main()
