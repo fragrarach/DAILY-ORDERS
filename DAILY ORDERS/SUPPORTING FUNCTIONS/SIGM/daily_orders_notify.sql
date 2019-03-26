aa6d99e3f59ab641c9e23e7869a4487c0543503c
Jan Zalobinski <39808183+fragrarach@users.noreply.github.com>
2019-03-26 16:40:31 -0400

    Added trigger for INSERT operations on 'invoicing' table.

--- DAILY ORDERS/SUPPORTING FUNCTIONS/SIGM/daily_orders_notify.sql
+++ DAILY ORDERS/SUPPORTING FUNCTIONS/SIGM/daily_orders_notify.sql
@@ -11,4 +11,10 @@ CREATE TRIGGER daily_orders_notify
   ON order_line
   FOR EACH ROW
   EXECUTE PROCEDURE daily_orders_notify();
-  
+
+DROP TRIGGER IF EXISTS daily_orders_notify ON invoicing;
+CREATE TRIGGER daily_orders_notify
+  AFTER INSERT
+  ON invoicing
+  FOR EACH ROW
+  EXECUTE PROCEDURE daily_orders_notify();
